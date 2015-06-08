65ed2ba9e69b1a1e219677c1906f1bc54c0b4171
AlmasB <almaslvl@gmail.com>
2015-06-08 19:23:41 +0500

    more types

---
+++ src/com/almasb/consume/Types.java
@@ -0,0 +1,25 @@
+package com.almasb.consume;
+
+import com.almasb.fxgl.entity.EntityType;
+import com.almasb.fxgl.entity.PropertyKey;
+
+public class Types {
+
+    public enum Type implements EntityType {
+        PLAYER, SPAWN_POINT, NEXT_LEVEL_POINT,
+        ENEMY, PLATFORM, BLOCK, TRIGGER, POWERUP
+    }
+
+    public enum Powerup {
+        RESTORE_HEALTH, RESTORE_MANA,   // powerups
+        INC_MAX_HEALTH, INC_MAX_MANA, INC_MANA_REGEN    // upgrades
+    }
+
+    public enum Platform {
+        DESTRUCTIBLE, INDESTRUCTIBLE
+    }
+
+    public enum Property implements PropertyKey {
+        SUB_TYPE
+    }
+}
