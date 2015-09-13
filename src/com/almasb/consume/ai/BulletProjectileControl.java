4d4b27998f5acee9cdacf6a335399c5d78b48c62
Ergo21 <jww14@uni.brighton.ac.uk>
2015-09-13 17:29:06 +0100

    Simple Update

--- src/com/almasb/consume/ai/BulletProjectileControl.java
+++ src/com/almasb/consume/ai/BulletProjectileControl.java
@@ -8,26 +8,27 @@ import com.almasb.fxgl.entity.FXGLEvent;
 
 public class BulletProjectileControl extends AbstractControl {
 
-    private boolean facingRight;
-    private Entity player;
+	private boolean facingRight;
+	private Entity player;
 
-    public BulletProjectileControl(Entity player) {
-        facingRight = player.getProperty("facingRight");
-        this.player = player;
-    }
+	public BulletProjectileControl(Entity player) {
+		facingRight = player.getProperty("facingRight");
+		this.player = player;
+	}
 
-    @Override
-    protected void initEntity(Entity entity) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	protected void initEntity(Entity entity) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public void onUpdate(Entity entity, long now) {
-        PhysicsControl control = entity.getControl(PhysicsControl.class);
-        control.moveX(facingRight ? (Speed.PROJECTILE + Speed.PROJECTILE/2): -(Speed.PROJECTILE + Speed.PROJECTILE/2));
+	@Override
+	public void onUpdate(Entity entity, long now) {
+		PhysicsControl control = entity.getControl(PhysicsControl.class);
+		control.moveX(
+				facingRight ? (Speed.PROJECTILE + Speed.PROJECTILE / 2) : -(Speed.PROJECTILE + Speed.PROJECTILE / 2));
 
-        if (Math.abs(entity.getTranslateX() - player.getTranslateX()) >= 350) {
-            entity.fireFXGLEvent(new FXGLEvent(Event.DEATH));
-        }
-    }
+		if (Math.abs(entity.getTranslateX() - player.getTranslateX()) >= 350) {
+			entity.fireFXGLEvent(new FXGLEvent(Event.DEATH));
+		}
+	}
 }
--- src/com/almasb/consume/ai/PhysicsControl.java
+++ src/com/almasb/consume/ai/PhysicsControl.java
@@ -9,8 +9,8 @@ import com.almasb.fxgl.entity.AbstractControl;
 import com.almasb.fxgl.entity.Entity;
 
 /**
- * Allows moving an entity with physics collision
- * rules, including gravity effect
+ * Allows moving an entity with physics collision rules, including gravity
+ * effect
  *
  * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
  * @version 1.0
@@ -18,55 +18,64 @@ import com.almasb.fxgl.entity.Entity;
  */
 public class PhysicsControl extends AbstractControl {
 
-    private Physics physics;
-    private Point2D velocity = new Point2D(0, 0);
+	private Physics physics;
+	private Point2D velocity = new Point2D(0, 0);
+	private int jumpStr;
 
-    public PhysicsControl(Physics physics) {
-        this.physics = physics;
-    }
+	public PhysicsControl(Physics physics) {
+		this.physics = physics;
+		jumpStr = Speed.PLAYER_JUMP;
+	}
 
-    @Override
-    protected void initEntity(Entity entity) {
-        entity.setProperty("jumping", false);
-        entity.setProperty(Property.ENABLE_GRAVITY, true);
-    }
+	@Override
+	protected void initEntity(Entity entity) {
+		entity.setProperty("jumping", false);
+		entity.setProperty(Property.ENABLE_GRAVITY, true);
+	}
 
-    @Override
-    public void onUpdate(Entity entity, long now) {
-    	physics.moveX(entity, (int)velocity.getX());
-        if (entity.<Boolean>getProperty(Property.ENABLE_GRAVITY)) {
-            velocity = velocity.add(0, Speed.GRAVITY_ACCEL);
-            if (velocity.getY() > Speed.GRAVITY_MAX)
-                velocity = new Point2D(velocity.getX(), Speed.GRAVITY_MAX);
-            
-        }
-        physics.moveY(entity, (int)velocity.getY());
-        
-    }
+	@Override
+	public void onUpdate(Entity entity, long now) {
+		physics.moveX(entity, (int) velocity.getX());
+		if (entity.<Boolean> getProperty(Property.ENABLE_GRAVITY)) {
+			velocity = velocity.add(0, Speed.GRAVITY_ACCEL);
+			if (velocity.getY() > Speed.GRAVITY_MAX)
+				velocity = new Point2D(velocity.getX(), Speed.GRAVITY_MAX);
 
-    public void moveX(int value) {
-        velocity = new Point2D(value, velocity.getY());
-    }
+		}
+		physics.moveY(entity, (int) velocity.getY());
+	}
 
-    public void moveY(int value) {
-    	velocity = new Point2D(velocity.getX(), value);
-    }
+	public void moveX(int value) {
+		velocity = new Point2D(value, velocity.getY());
+	}
 
-    public void jump() {
-        if (entity.<Boolean>getProperty("jumping"))
-            return;
+	public void moveY(int value) {
+		velocity = new Point2D(velocity.getX(), value);
+	}
 
-        entity.setProperty("jumping", true);
-        velocity = new Point2D(velocity.getX(), -Speed.PLAYER_JUMP);
-    }
-    
-    public void climb(int clSpd) {
-    	if(entity.getProperty("climbing") != null && entity.<Boolean>getProperty("climbing")){
-    		physics.moveY(entity, clSpd);
-    	}
-    }
+	public void jump() {
+		if (entity.<Boolean> getProperty("jumping"))
+			return;
 
-    public Point2D getVelocity() {
-        return velocity;
-    }
+		entity.setProperty("jumping", true);
+		velocity = new Point2D(velocity.getX(), -jumpStr);
+	}
+
+	public void setJump(int jump) {
+		jumpStr = jump;
+	}
+
+	public int getJump() {
+		return jumpStr;
+	}
+
+	public void climb(int clSpd) {
+		if (entity.getProperty("climbing") != null && entity.<Boolean> getProperty("climbing")) {
+			physics.moveY(entity, clSpd);
+		}
+	}
+
+	public Point2D getVelocity() {
+		return velocity;
+	}
 }
